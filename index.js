ab80c13279edc3304513cecd236e9dc7d21b9b2d
Léandre DA SILVA <62658427+leandredasilva@users.noreply.github.com>
2022-06-12 10:46:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World from AKS!')
+  res.send('🎉 Hello World from AKS!')
 })
 
 app.listen(8080, function () {
